28fb8aaa57af975eb01508ef07bb57666466259b
jaumeol <joliverlafont@gmail.com>
2024-11-27 09:42:19 +0100

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'la meva aplicació angular';
+  title = 'la meva primera aplicació angular';
 }
